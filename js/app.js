9bddb618e23362e7ba64e3d10ef49c8efcfe6da7
Nathan Chan <chan.nathan.th@gmail.com>
2019-08-17 15:50:44 -0700

    One enemy implemented. Regenerates a little quickly.

--- js/app.js
+++ js/app.js
@@ -6,7 +6,29 @@ var Enemy = function() {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+
+    this.x = 0;
+
+    this.y = initialY();
+
+    this.speed = 500;
 };
+
+speeds = {
+  fast: 500,
+  medium: 100,
+  slow: 50
+}
+
+function initialY() {
+  let row = Math.floor(Math.random() * 3);
+  if (row == 3) {
+    //in unlikely case that 1 is drawn by Math.random()
+    row = 2;
+  }
+
+  return row * 80 + 64;
+}
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
@@ -14,7 +36,13 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-};
+    if (this.x <= ctx.canvas.width) {
+      this.x += dt * this.speed
+    } else {
+      this.x = 0;
+      this.y = initialY();
+    }
+}
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
@@ -25,11 +53,25 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 
+class Player {
+  constructor() {}
+
+  update() {}
+
+  render() {}
+
+  handleInput(key) {}
+}
+
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
+const enemy = new Enemy();
+const allEnemies = [enemy];
+
+const player = new Player();
 
 
 // This listens for key presses and sends the keys to your
